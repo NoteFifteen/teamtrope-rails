b03aa53b14b28c85e516fa95caa6a5852f73f150
Justin Jeffress <justin.jeffress@booktrope.com>
2014-12-04 18:35:05 -0800

    Updated default_url for avatar
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 	has_attached_file :avatar,
 		:styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>"},
 		:convert_options => { :large => "-quality 100", :medium => '-quality 100', :thumb => '-quality 100' },
-		:default_url => "/images/:style/missing.png",
+		:default_url => "/images/:style/missing.gif",
 		:processors => [:cropper] 
 		
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
