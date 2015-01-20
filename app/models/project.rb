036b12ae9082075631d17a5285e1330d88887f4c
Justin Jeffress <justin.jeffress@booktrope.com>
2015-01-19 22:54:06 -0800

    Added checks for required roles to Project
    Added method team_complete? which returns true when a project has fulfilled all of its required roles.
    
    Added a function that generates has_insert_role_name_here? functions based on the roles defined in the roles table.
    
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,6 +9,30 @@ class Project < ActiveRecord::Base
 	
 	has_many :current_tasks
 	
-	#validates :manuscript_original, presence: true
+	def team_complete?
+		required_roles = project_type.required_roles.ids
+		team_members = team_memberships.map { |member| member.role_id   }
+		
+		required_roles.all? {| required_role | team_members.include? required_role }
+	end
+	
+	
+	def self.provide_methods_for(role)
+		Project.class_eval %Q{	
+			def has_#{role.downcase.gsub(/ /, "_")}?
+				team_memberships.where(role_id: Role.where(name: "#{role}")).count > 0
+			end
+		}
+		Project.class_eval %Q{
+			def #{role.downcase.gsub(/ /, "_").pluralize}
+				team_memberships.where(role_id: Role.where(name: "#{role}"))
+			end
+		}
+	end
+	
+	Role.all.map{ |role| role.name  }.each do | role |
+		provide_methods_for(role)
+	end
+	
 	
 end
