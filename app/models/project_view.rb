f72dc8c739397a9928f4e1ea3cca4c5734114744
Justin Jeffress <justin.jeffress@booktrope.com>
2015-01-20 15:14:12 -0800

    Implemented Nested Forms ProjectView
    ProjectView accepts nested attributes for Phases
    
    Phases accepts nested attributes for Tabs
    
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/models/project_view.rb
+++ app/models/project_view.rb
@@ -2,4 +2,8 @@ class ProjectView < ActiveRecord::Base
   belongs_to :project_type
   has_many :phases
   
+  accepts_nested_attributes_for :phases, reject_if: :all_blank, allow_destroy: true  
+  
+  validates :project_type, presence: true
+  
 end
