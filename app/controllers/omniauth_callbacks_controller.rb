82eaaace85f6b51fc1286d0c45ae9daae0e2a170
Justin Jeffress <justin.jeffress@booktrope.com>
2015-04-10 17:12:11 -0700

    Task 170 Convert login to use devise.
    https://booktrope.acunote.com/projects/47888/tasks/170
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,15 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def wordpress_hosted
+
+    #You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_wordpress_oauth2(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Wordpress Oauth2"
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    else
+      session["devise.wordpress_oauth2_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+end
