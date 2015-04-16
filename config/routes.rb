6f43035ea1fa4e8aeb33e01780fa59b1f007989e
Justin Jeffress <justin.jeffress@booktrope.com>
2015-04-15 18:25:36 -0700

    WP Import
    initial commit
    
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
+  get 'wordpress_import/import'
+  post 'wordpress_import/upload'
+
+
   resources :marketing_expenses
 
   resources :publication_fact_sheets
