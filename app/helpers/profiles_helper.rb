5cbed13c1021dee67ae85d85e3f29e8fef3eef22
Brian Ronald <brronald@gmail.com>
2015-04-21 14:46:56 -0700

    Added a helper for obtaining the user's Avatar should we want to support Gravatar in the future.  (I was told users don't like the default) Added Author to a Project's member list. Changed the Proof Reader's background color to white since it's next to editor and was the same color.

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,18 @@
 module ProfilesHelper
+
+  def avatar_url user, image_tag_dimensions, gravatar_size
+    # If don't have an avatar present, try Gravatar
+    if(! user.profile.avatar.present?)
+      user.profile.avatar.url(image_tag_dimensions)
+      #gravatar_url(user, gravatar_size)
+    else
+      user.profile.avatar.url(image_tag_dimensions)
+    end
+  end
+
+  def gravatar_url user, gravatar_size = 150
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    #d=wavatar&
+    "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{gravatar_size}"
+  end
 end
