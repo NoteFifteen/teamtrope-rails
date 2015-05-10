b8a90ad1452e30f64eedc899daf74f8824e960e2
Justin Jeffress <justin.jeffress@booktrope.com>
2015-05-10 16:40:19 -0700

    Task 368 - My books list distinct pagination fixed
    https://booktrope.acunote.com/tasks/368
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,7 +6,7 @@ module ProjectsHelper
     @grid_title = "The Grid"
     unless filter_by.nil? || !filters.has_key?(filter_by.to_sym)
       if filter_by.to_sym == :all
-        @projects = Project.page(params[:page]) if Project.count > 2000
+        @projects = Project.page(params[:page]) if Project.count > 500
         @projects ||= Project.all
       else
         @grid_title =  filter_by.to_s.humanize.gsub(/_/," ")
@@ -15,7 +15,8 @@ module ProjectsHelper
       end
     else
       @grid_title = "My Books"
-      @projects = current_user.projects.order(title: :asc)
+      # We only want the book to show up once in the list so we use distinct.
+      @projects = current_user.projects.order(title: :asc).distinct
         .includes(:team_memberships => [:member, :role])
       @projects = @projects.page(params[:page]) if @projects.count > 500
     end
