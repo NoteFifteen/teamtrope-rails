df5e20747eeecc0e0598f275f9973bd4ea303b08
Justin Jeffress <justin.jeffress@booktrope.com>
2015-06-09 16:53:24 -0700

    Task 400 - Added Nook and Apple Sales to Analytics
    https://booktrope.acunote.com/projects/47888/tasks/400
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,32 +3,58 @@ class AnalyticsController < ApplicationController
   def show
     @project = Project.friendly.find(params[:project_id])
 
-    # Test with ASIN B00GJ1XZSS
-    if @project.control_number.try(:asin)
-      sales_result = Parse::Cloud::Function.new('getSalesDataForAsin').call('asin'=> @project.control_number.asin)
-      rank_result = Parse::Cloud::Function.new('getCrawlDataForAsin').call('asin'=> @project.control_number.asin)
+    response = { no_data: true }
 
-      response = {
+    case params[:channel]
+    when "amazon"
+
+      # Test with ASIN B00GJ1XZSS
+      if @project.control_number.try(:asin)
+        sales_result = Parse::Cloud::Function.new('getSalesDataForAsin').call('asin'=> @project.control_number.asin)
+        rank_result = Parse::Cloud::Function.new('getCrawlDataForAsin').call('asin'=> @project.control_number.asin)
+        response = {
           sales: {
-              title: sales_result['title'],
-              data: sales_result['crawl'].map{ |crawl_data| [Time.parse(crawl_data['crawlDate']['iso']).utc.to_i * 1000, crawl_data['dailySales']] }.inspect
+            title: sales_result['title'],
+            data: sales_result['crawl'].map{ |crawl_data| [Time.parse(crawl_data['crawlDate']['iso']).utc.to_i * 1000, crawl_data['dailySales']] }.inspect
           },
           rank: {
-              title: rank_result['title'],
-              price_data: rank_result['crawl'].reject{|crawl_data| !crawl_data['got_price'] }.map{ |crawl_data|  [Time.parse(crawl_data['crawl_date']['iso']).utc.to_i*1000, crawl_data['kindle_price']] }.inspect,
-              rank_data: rank_result['crawl'].map{|crawl_data| [Time.parse(crawl_data['crawl_date']['iso']).utc.to_i*1000, crawl_data['sales_rank']]}.inspect
-          }
-      }
+            title: rank_result['title'],
+            price_data: rank_result['crawl'].reject{|crawl_data| !crawl_data['got_price'] }.map{ |crawl_data|  [Time.parse(crawl_data['crawl_date']['iso']).utc.to_i*1000, crawl_data['kindle_price']] }.inspect,
+            rank_data: rank_result['crawl'].map{|crawl_data| [Time.parse(crawl_data['crawl_date']['iso']).utc.to_i*1000, crawl_data['sales_rank']]}.inspect
+          },
+          no_data: false
+        }
+      end
+    when "nook"
+      if @project.control_number.try(:parse_id)
+        sales_result = Parse::Cloud::Function.new('getSalesDataForNook').call('book' => @project.control_number.parse_id)
+        response = {
+          sales: {
+            title: sales_result['title'].nil?? @project.title : sales_result['title'],
+            data: sales_result['crawl'].map{ |crawl_data| [Time.parse(crawl_data['crawlDate']['iso']).utc.to_i * 1000, crawl_data['nookSales']]}.inspect
+          },
+          no_data: false
+        }
+      end
+    when "apple"
+      if @project.control_number.try(:parse_id)
+        sales_result = Parse::Cloud::Function.new('getSalesDataForApple').call('book' => @project.control_number.parse_id)
+        puts sales_result['title']
+        response = {
+          sales: {
+            title: sales_result['title'].nil?? @project.title : sales_result['title'],
+            data: sales_result['crawl'].map{ |crawl_data| [Time.parse(crawl_data['crawlDate']['iso']).utc.to_i * 1000, crawl_data['appleSales']]}.inspect
+          },
+          no_data: false
+        }
+      end
     else
-      response = {
-          no_data: true
-      }
+      response[:error] = { messages: ["channel #{params[:channel]} is not supported"] }
     end
 
     # respond_to do |format|
     #   format.json { render json: response }
     # end
-
     render json: response
 
   end
