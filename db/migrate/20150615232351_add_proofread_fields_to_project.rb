9c0c627f806d568be6f50027a6a542a5a4d6660a
Brian Ronald <brronald@gmail.com>
2015-06-17 14:38:41 -0700

    Add column type to dropped column to make migration reversible

--- db/migrate/20150615232351_add_proofread_fields_to_project.rb
+++ db/migrate/20150615232351_add_proofread_fields_to_project.rb
@@ -7,7 +7,7 @@ class AddProofreadFieldsToProject < ActiveRecord::Migration
     add_column :projects, :previously_published_title, :string
 
     # Unused column, and I'd rather use separate fields
-    remove_column :projects, :prev_publisher_and_date
+    remove_column :projects, :prev_publisher_and_date, :string
 
     add_column :projects, :previously_published_year, :integer
     add_column :projects, :previously_published_publisher, :string
