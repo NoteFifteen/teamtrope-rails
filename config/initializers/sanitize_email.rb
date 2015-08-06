2c97bdbd1a5031799996ffce0561d2617de01116
Brian Ronald <brronald@gmail.com>
2015-08-06 13:54:13 -0700

    Add staging to the proc enable for Sanitize Email

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -6,7 +6,7 @@ SanitizeEmail::Config.configure do |config|
   config[:sanitized_cc] =         Figaro.env.mail_sanitized_cc
   config[:sanitized_bcc] =        Figaro.env.mail_sanitized_bcc
   # run/call whatever logic should turn sanitize_email on and off in this Proc:
-  config[:activation_proc] =      Proc.new { %w(development test).include?(Rails.env) }
+  config[:activation_proc] =      Proc.new { %w(development staging test).include?(Rails.env) }
   config[:use_actual_email_prepended_to_subject] = false        # or false
   config[:use_actual_environment_prepended_to_subject] = true   # or false
   config[:use_actual_email_as_sanitized_user_name] = true       # or false
