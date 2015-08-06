fc51da7e19030aac5f9030824a5109e97c3a2ba5
Justin Jeffress <justin.jeffress@booktrope.com>
2015-08-06 16:13:35 -0700

    Draft Blurbs Scaffold
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/controllers/draft_blurbs_controller.rb
+++ app/controllers/draft_blurbs_controller.rb
@@ -1,5 +1,8 @@
 class DraftBlurbsController < ApplicationController
+  before_action :signed_in_user
+  before_action :booktrope_staff, except: :show
   before_action :set_draft_blurb, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
@@ -27,8 +30,21 @@ class DraftBlurbsController < ApplicationController
   end
 
   def update
-    @draft_blurb.update(draft_blurb_params)
-    respond_with(@draft_blurb)
+    respond_to do | format |
+      if @draft_blurb.update(draft_blurb_params)
+
+        @project.create_activity :edited_blurb, owner: current_user,
+                                  parameters: { text: 'Edited', object_id: @draft_blurb.id, form_data: params[:draft_blurb].to_s}
+
+        ProjectMailer.submit_blurb(@project, current_user)
+
+        format.html { redirect_to @draft_blurb, notice: 'Draft Blurb was successfully updated.' }
+        format.json { render :show, status: :ok, location: @draft_blurb }
+      else
+        format.html { render :edit }
+        format.json { render json: @draft_blurb.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -41,7 +57,11 @@ class DraftBlurbsController < ApplicationController
       @draft_blurb = DraftBlurb.find(params[:id])
     end
 
+    def set_project
+      @project = @draft_blurb.project
+    end
+
     def draft_blurb_params
-      params.require(:draft_blurb).permit(:project_id, :draft_blurb)
+      params.require(:draft_blurb).permit(:draft_blurb)
     end
 end
