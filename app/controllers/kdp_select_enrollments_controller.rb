0eb8cfa853c415fb4f952b3a5620c60b5d195fcd
Justin Jeffress <justin.jeffress@booktrope.com>
2015-08-06 16:15:55 -0700

    KDP Select Enrollment Scaffold
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/controllers/kdp_select_enrollments_controller.rb
+++ app/controllers/kdp_select_enrollments_controller.rb
@@ -1,5 +1,8 @@
 class KdpSelectEnrollmentsController < ApplicationController
+  before_action :signed_in_user
+  before_action :booktrope_staff, except: :show
   before_action :set_kdp_select_enrollment, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
@@ -27,7 +30,12 @@ class KdpSelectEnrollmentsController < ApplicationController
   end
 
   def update
-    @kdp_select_enrollment.update(kdp_select_enrollment_params)
+    if @kdp_select_enrollment.update(kdp_select_enrollment_params)
+        @project.create_activity :edited_kdp_select_enrollment, owner: current_user,
+                                 parameters: { text: 'Edited', object_id: @kdp_select_enrollment.id, form_data: kdp_select_enrollment_params.to_s}
+
+    end
+
     respond_with(@kdp_select_enrollment)
   end
 
@@ -41,7 +49,11 @@ class KdpSelectEnrollmentsController < ApplicationController
       @kdp_select_enrollment = KdpSelectEnrollment.find(params[:id])
     end
 
+    def set_project
+      @project = @kdp_select_enrollment.project
+    end
+
     def kdp_select_enrollment_params
-      params.require(:kdp_select_enrollment).permit(:project_id, :member_id, :enrollment_date, :update_type, :update_data)
+      params.require(:project).require(:kdp_select_enrollment_attributes).permit(:project_id, :member_id, :enrollment_date, :update_type, :update_data)
     end
 end
--- app/models/kdp_select_enrollment.rb
+++ app/models/kdp_select_enrollment.rb
@@ -6,4 +6,10 @@ class KdpSelectEnrollment < ActiveRecord::Base
   validates :member_id, presence: true
   validates :enrollment_date, presence: true
 
+  UpdateTypes = [
+      ['countdown_deal', 'Countdown Deal'],
+      ['free_book_promo', 'Free Book Promotion'],
+      ['remove_from_kdp', 'Remove this book from KDP Select at the end of the current 90 day period'],
+  ]
+
 end
