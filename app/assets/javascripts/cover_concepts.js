348bcead1cf2eb47be91f21849b6d115180de693
Alex Lewin <alex.lewin@booktrope.com>
2015-10-18 15:59:53 -0700

    TTR-105 almost (checkbox validation not working)

--- app/assets/javascripts/cover_concepts.js
+++ app/assets/javascripts/cover_concepts.js
@@ -4,6 +4,65 @@
  * This file contains javascript relating to some if not all of the Cover Concept related forms
  */
 
+
+$(document).ready(function() {
+
+    // deal with "Other"
+    $('#cover_concept_image_source_other_input').focus(function() {
+        $('#cover_concept_image_source_other').prop('checked', true);
+        if (this.value == 'Other') {
+            this.value = '';
+        }
+    });
+    $('#cover_concept_image_source_other_input').blur(function() {
+        if (this.value == '') {
+            this.value = 'Other';
+        }
+    });
+
+    $('#cover_concept_image_source_other').change(function() {
+        if (this.checked) {
+            $('#cover_concept_image_source_other_input').focus();
+        }
+    });
+
+    // validation
+    $('#cover_concept_upload').validate({
+        errorElement: "div",
+        errorPlacement: function(error, element) {
+            error.appendTo("div#cover_concept_upload_errors");
+        },
+        rules: {
+            'project[cover_concepts][image_source]': {
+                required: true
+            },
+            'project[cover_concepts][signoffs]': {
+                required: true,
+                minlength: 2
+            },
+            'cover_concept_image_source_other_input': {
+                otherSourceValid: true
+            }
+        },
+        messages: {
+            'project[cover_concepts][image_source]': {
+                required: 'You must specify a source for the cover concept image.'
+            },
+            'project[cover_concepts][signoffs]': {
+                required: 'You must confirm all the signoff items.',
+                minlength: 'You must confirm all the signoff items.'
+            }
+        }
+    });
+
+    $.validator.addMethod("otherSourceValid", function(value) {
+        var checked =  $('#cover_concept_image_source_other').is(':checked');
+        return !checked || (value != "Other" && value != "")
+    }, 'Please enter a source when selecting "Other".');
+});
+
+
+
 /** Upload Cover Concept Form **/
 $(function() {
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926214432) do
+ActiveRecord::Schema.define(version: 20151016195332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20150926214432) do
     t.string   "unapproved_cover_concept_content_type"
     t.integer  "unapproved_cover_concept_file_size"
     t.datetime "unapproved_cover_concept_updated_at"
+    t.string   "image_source"
   end
 
   add_index "cover_concepts", ["project_id"], name: "index_cover_concepts_on_project_id", using: :btree
