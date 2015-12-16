8512d34effb6ca6e5c6c8986d185f3431079f5b4
Brian Ronald <brronald@gmail.com>
2015-12-15 16:01:36 -0800

    Add some try's when prepopulating the model in case we're pulling up a project that doesn't have some of the models

--- app/models/netgalley_submission.rb
+++ app/models/netgalley_submission.rb
@@ -53,10 +53,10 @@ class NetgalleySubmission < ActiveRecord::Base
   def custom_init
     return if project.nil?
 
-    self.title ||= project.final_title
+    self.title ||= (project.final_title) ? project.final_title : project.title
     self.author_name ||= project.authors.first.member.display_name
-    self.retail_price ||= project.publication_fact_sheet.ebook_price
-    self.publication_date ||= project.published_file.publication_date
+    self.retail_price ||= project.try(:publication_fact_sheet).try(:ebook_price)
+    self.publication_date ||= project.try(:published_file).try(:publication_date)
     self.isbn ||= project.try(:control_number).try(:epub_isbn)
     self.blurb ||= project.try(:draft_blurb).try(:draft_blurb)
   end
