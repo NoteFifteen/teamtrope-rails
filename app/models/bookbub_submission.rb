785398542d59c6c25c2994c9a9d696b4ee41bdea
Brian Ronald <brronald@gmail.com>
2016-01-04 15:53:10 -0800

    TTR-106 - Changes based on UAT Feedback

--- app/models/bookbub_submission.rb
+++ app/models/bookbub_submission.rb
@@ -10,7 +10,6 @@ class BookbubSubmission < ActiveRecord::Base
     self.author ||= project.authors.first.member.display_name
     self.asin ||= project.try(:control_number).try(:asin)
     self.current_price ||= project.try(:publication_fact_sheet).try(:ebook_price)
-    self.num_pages ||= project.try(:layout).try(:final_page_count)
 
   end
 
