dc3339194829469ef47ed5d27e0177b562e35135
Justin Jeffress <justin.jeffress@booktrope.com>
2016-03-30 14:39:47 -0700

    Implemented CSV Export
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- app/models/monthly_published_book.rb
+++ app/models/monthly_published_book.rb
@@ -2,6 +2,16 @@ class MonthlyPublishedBook < ActiveRecord::Base
   def self.csv_export
     require 'csv'
     csv_string = CSV.generate do | csv |
+      csv << [ "Year", "Month", "Number Published", "Total Published" ]
+
+      all.order(report_date: :desc).each do | monthly_published_book |
+        csv << [ monthly_published_book.report_date.strftime("%Y"),
+          monthly_published_book.report_date.strftime("%m"),
+          monthly_published_book.published_monthly,
+          monthly_published_book.published_total
+        ]
+      end
+
     end
   end
 end
